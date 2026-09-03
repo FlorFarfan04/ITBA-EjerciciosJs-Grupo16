fcc6b8b3273ce1a2c6cd01021893514ca700f269
FlorFarfan <florfarfan04@gmail.com>
2026-09-02 21:13:55 -0300

    Agrego ejercicio 3

---
+++ ejercicio3.js
@@ -0,0 +1,14 @@
+let totalCompra = 0;
+let respuesta = confirm('¿Desea agregar un producto al carrito?');
+
+while (respuesta === true) {
+    const valorProducto = parseFloat(prompt('Ingrese el valor del producto:'));
+    if(isNaN(valorProducto)) {
+        alert('El valor ingresado no es un número válido');
+    } else {
+        totalCompra += valorProducto;
+    }
+    respuesta = confirm('¿Desea agregar un producto al carrito?');
+}
+
+alert(`El total de su compra es: ${totalCompra}`);
